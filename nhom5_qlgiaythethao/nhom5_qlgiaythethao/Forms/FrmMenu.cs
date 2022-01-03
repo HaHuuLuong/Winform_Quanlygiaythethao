699b59971472c2d887bb8810efd4d46f0ecf8610
unknown <chamcham15702@gmail.com>
2022-01-04 00:04:48 +0700

    add

---
+++ nhom5_qlgiaythethao/nhom5_qlgiaythethao/Forms/FrmMenu.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace nhom5_qlgiaythethao
+{
+    public partial class FrmMenu : Form
+    {
+        public FrmMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmMenu_Load(object sender, EventArgs e)
+        {
+            Class.Functions.Connect();
+        }
+
+        private void mnuThoat_Click(object sender, EventArgs e)
+        {
+            Class.Functions.Disconnect();
+            Application.Exit();
+        }
+
+    }
+}
